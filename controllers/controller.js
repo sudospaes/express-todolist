501a1367708e28333c4702637d5cae0b0fdb67e8
Sudo Space <abolfazleffati1381@gmail.com>
2023-07-18 23:00:21 +0330

    Add Finish all & Remove all

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,16 @@
 const Todo = require("../model/todo");
 
+const getTodos = (req, res) => {
+  Todo.findAll()
+    .then((todos) => {
+      res.render("index", { title: "Main Page", todos });
+    })
+    .catch((err) => {
+      res.render("404", { title: "404 - Not Found" });
+      console.log(err);
+    });
+};
+
 const addTodo = (req, res) => {
   if (!req.body.todo) {
     res.redirect("/");
@@ -18,6 +29,12 @@ const removeTodo = (req, res) => {
   });
 };
 
+const removeAllTodos = (req, res) => {
+  Todo.truncate()
+    .then(() => res.redirect("/"))
+    .catch((err) => console.log(err));
+};
+
 const finishingTodo = (req, res) => {
   Todo.findByPk(req.params.id)
     .then((todo) => {
@@ -27,8 +44,17 @@ const finishingTodo = (req, res) => {
     .then(() => res.redirect("/"));
 };
 
+const finishAllTodo = (req, res) => {
+  Todo.update({ finished: true }, { where: { finished: false } })
+    .then(() => res.redirect("/"))
+    .catch((err) => console.log(err));
+};
+
 module.exports = {
+  getTodos,
   addTodo,
   removeTodo,
+  removeAllTodos,
   finishingTodo,
+  finishAllTodo,
 };
--- routes/router.js
+++ routes/router.js
@@ -5,22 +5,17 @@ const controller = require("../controllers/controller");
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  Todo.findAll()
-    .then((todos) => {
-      res.render("index", { title: "Main Page", todos });
-    })
-    .catch((err) => {
-      res.render("404", { title: "404 - Not Found" });
-      console.log(err);
-    });
-});
+router.get("/", controller.getTodos);
 
 router.post("/add-todo", controller.addTodo);
 
 router.post("/remove-todo/:id", controller.removeTodo);
 
+router.post("/remove-all", controller.removeAllTodos);
+
 router.post("/finishing-todo/:id", controller.finishingTodo);
+
+router.post("/finishing-all/", controller.finishAllTodo);
 
 router.use((req, res) => {
   res.render("404", { title: "404 - Not Found" });
